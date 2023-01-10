b958b28b78caba0deb936dc8e327df49265b2aa9
Veit Heller <veit@veitheller.de>
2023-01-10 19:54:17 +0100

    make compilable

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -1,27 +1,32 @@
 #![allow(non_snake_case)]
 extern crate notify;
 
-use notify::{PollWatcher, RecursiveMode, watcher};
-use std::sync::mpsc::channel;
-use value_box::{ValueBox};
+use notify::{PollWatcher, RecursiveMode, Watcher, DebouncedEvent};
+use std::sync::mpsc::{channel, Sender, Receiver};
+use std::time::Duration;
+use value_box::{ValueBox, ValueBoxPointer};
+use string_box::StringBox;
+
 
 #[no_mangle]
-pub fn filewatcher_create_channel() -> *mut ValueBox<(Sender<DebouncedEvent>, Receiver<DebouncedEvent>)> {
-    ValueBox::new(channel()).into_raw()
+pub fn filewatcher_create_watcher() -> *mut ValueBox<(PollWatcher, Receiver<DebouncedEvent>)> {
+    let (tx, rx) = channel();
+    match PollWatcher::new(tx, Duration::from_secs(10)) {
+        Ok(watcher) => ValueBox::new((watcher, rx)).into_raw(),
+        Err(_) => std::ptr::null_mut()
+    }
 }
 
 #[no_mangle]
-pub fn filewatcher_destroy_channel(ptr: *mut ValueBox<(Sender<DebouncedEvent>, Receiver<DebouncedEvent>)>) {
-    ptr.release();
-}
-
-#[no_mangle]
-pub fn filewatcher_create_watcher(ptr: *mut ValueBox<(Sender<DebouncedEvent>, Receiver<DebouncedEvent>)>) -> *mut ValueBox<PollWatcher> {
-    ptr.to_ref().and_then(|tuple|
-        match watcher(tuple.0, RecursiveMode::Recursive) {
-            Ok(watcher) => ValueBox::new(watcher).into_raw(),
-            Err(_) => std::ptr::null_mut(),
-        }).log()
+pub fn filewatcher_watcher_watch(ptr: *mut ValueBox<PollWatcher>, path_ptr: *mut ValueBox<StringBox>) {
+    match ptr.to_ref() {
+      Ok(mut watcher) =>
+        match path_ptr.to_ref() {
+            Ok(path) => watcher.watch(path.to_string(), RecursiveMode::Recursive).unwrap(),
+            Err(_) => ()
+        }
+      Err(_) => ()
+    }
 }
 
 #[no_mangle]
@@ -30,11 +35,18 @@ pub fn filewatcher_destroy_watcher(ptr: *mut ValueBox<PollWatcher>) {
 }
 
 #[no_mangle]
+pub fn filewatcher_destroy_receiver(ptr: *mut ValueBox<Receiver<DebouncedEvent>>) {
+    ptr.release();
+}
+
+#[no_mangle]
 pub fn filewatcher_receive_event(ptr: *mut ValueBox<(Sender<DebouncedEvent>, Receiver<DebouncedEvent>)>) -> *mut ValueBox<DebouncedEvent> {
-    ptr.to_ref().and_then(|tuple|
+    match ptr.to_ref() {
+      Ok(tuple) =>
         match tuple.1.recv() {
            Ok(event) => ValueBox::new(event).into_raw(),
            Err(_) => std::ptr::null_mut(),
         }
-    ).log()
+        Err(_) => std::ptr::null_mut()
+    }
 }
