37d4865d66ab6f90902753437562c371388de8a2
Veit Heller <veit@veitheller.de>
2023-02-02 22:52:31 +0100

    update for newest version of libraries

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -4,9 +4,10 @@ extern crate env_logger;
 extern crate notify;
 #[macro_use]
 extern crate phlow;
+extern crate phlow_ffi;
 extern crate phlow_extensions;
 
-use notify::{Event, EventKind, EventHandler, RecommendedWatcher, RecursiveMode, Watcher};
+use notify::{Event, EventHandler, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
 use phlow_extensions::CoreExtensions;
 use std::collections::VecDeque;
 use std::error::Error;
@@ -15,6 +16,11 @@ use std::sync::{Arc, Mutex};
 use string_box::StringBox;
 use value_box::ReturnBoxerResult;
 use value_box::{ValueBox, ValueBoxPointer};
+
+// Re-export everything from the `value_box_ffi` and `phlow_ffi` in order to tell Rust to include
+// the corresponding `no_mangle` functions.
+pub use phlow_ffi::*;
+pub use value_box_ffi::*;
 
 type AffectedPaths = Vec<PathBuf>;
 
@@ -118,16 +124,19 @@ import_extensions!(FileWatcherExtensions, CoreExtensions);
 #[phlow::extensions(FileWatcherExtensions, PharoWatcher)]
 impl WatcherExtensions {
     #[phlow::view]
-    pub fn information_for(_this: &PharoWatcher, view: impl phlow::PhlowView) -> impl phlow::PhlowView {
+    pub fn information_for(
+        _this: &PharoWatcher,
+        view: impl phlow::PhlowView,
+    ) -> impl phlow::PhlowView {
         view.list()
             .title("Information")
-            .items(|watcher: &PharoWatcher, _object| {
-                phlow_all!(vec![("Queue size", phlow!(watcher.queue_size()))])
+            .items::<PharoWatcher>(|watcher| {
+                phlow_all!(vec![("Queue size", watcher.queue_size())])
             })
-            .item_text(|each: &(&str, phlow::PhlowObject), _object| {
+            .item_text::<(&str, usize)>(|each| {
                 format!("{}: {}", each.0, each.1.to_string())
             })
-            .send(|each: &(&str, phlow::PhlowObject), _object| each.1.clone())
+            .send::<(&str, usize)>(|each| phlow!(each.1.clone()))
     }
 }
 
@@ -149,29 +158,25 @@ pub extern "C" fn filewatcher_watcher_watch(
     ptr: *mut ValueBox<PharoWatcher>,
     path_ptr: *mut ValueBox<StringBox>,
 ) {
-    ptr.to_ref()
-        .and_then(|mut watcher| {
-            path_ptr.to_ref().and_then(|path| {
-                watcher
-                    .watch(Path::new(&path.to_string()))
-                    .map_err(|error| (Box::new(error) as Box<dyn Error>).into())
-            })
+    ptr.with_mut(|watcher| {
+        path_ptr.with_ref(|path| {
+            watcher
+                .watch(Path::new(&path.to_string()))
+                .map_err(|error| (Box::new(error) as Box<dyn Error>).into())
         })
-        .log();
+    })
+    .log();
 }
 
 #[no_mangle]
 pub extern "C" fn filewatcher_watcher_poll(
     ptr: *mut ValueBox<PharoWatcher>,
 ) -> *mut ValueBox<Event> {
-    match ptr.to_ref() {
-        Ok(watcher) =>
-            match watcher.poll_event() {
-                Some(event) => ValueBox::new(event).into_raw(),
-                None => std::ptr::null_mut()
-            }
-        Err(_) => std::ptr::null_mut()
-    }
+    ptr.with_ref_ok(|watcher| match watcher.poll_event() {
+        Some(event) => ValueBox::new(event).into_raw(),
+        None => std::ptr::null_mut(),
+    })
+    .unwrap_or_else(|_| std::ptr::null_mut())
 }
 
 #[no_mangle]
@@ -185,27 +190,23 @@ impl EventExtensions {
     pub fn information_for(_this: &Event, view: impl phlow::PhlowView) -> impl phlow::PhlowView {
         view.list()
             .title("Information")
-            .items(|event: &Event, _object| {
+            .items::<Event>(|event| {
                 phlow_all!(vec![
                     ("Event type", phlow!(event.kind.clone())),
                     ("Event paths", phlow!(event.paths.clone())),
                 ])
             })
-            .item_text(|each: &(&str, phlow::PhlowObject), _object| {
+            .item_text::<(&str, &phlow::PhlowObject)>(|each| {
                 format!("{}: {}", each.0, each.1.to_string())
             })
-            .send(|each: &(&str, phlow::PhlowObject), _object| each.1.clone())
+            .send::<(&str, &phlow::PhlowObject)>(|each| phlow!(each.1.clone()))
     }
 }
 
 #[no_mangle]
-pub extern "C" fn filewatcher_event_kind(
-    ptr: *mut ValueBox<Event>,
-) -> *mut ValueBox<EventKind> {
-    match ptr.to_ref() {
-        Ok(event) => ValueBox::new(event.kind.clone()).into_raw(),
-        Err(_) => std::ptr::null_mut()
-    }
+pub extern "C" fn filewatcher_event_kind(ptr: *mut ValueBox<Event>) -> *mut ValueBox<EventKind> {
+    ptr.with_ref_ok(|event| ValueBox::new(event.kind.clone()).into_raw())
+        .unwrap_or_else(|_| std::ptr::null_mut())
 }
 
 #[no_mangle]
@@ -219,77 +220,47 @@ pub extern "C" fn filewatcher_destroy_eventkind(ptr: *mut ValueBox<EventKind>) {
 }
 
 #[no_mangle]
-pub extern "C" fn filewatcher_eventkind_is_any(
-    ptr: *mut ValueBox<EventKind>
-) -> bool {
-    match ptr.to_ref() {
-        Ok(eventkind) =>
-            match *eventkind {
-                EventKind::Any => true,
-                _ => false
-            }
-        Err(_) => false
-    }
+pub extern "C" fn filewatcher_eventkind_is_any(ptr: *mut ValueBox<EventKind>) -> bool {
+    ptr.with_ref_ok(|eventkind| match *eventkind {
+        EventKind::Any => true,
+        _ => false,
+    })
+    .unwrap_or(false)
 }
 
 #[no_mangle]
-pub extern "C" fn filewatcher_eventkind_is_access(
-    ptr: *mut ValueBox<EventKind>
-) -> bool {
-    match ptr.to_ref() {
-        Ok(eventkind) => eventkind.is_access(),
-        Err(_) => false
-    }
+pub extern "C" fn filewatcher_eventkind_is_access(ptr: *mut ValueBox<EventKind>) -> bool {
+    ptr.with_ref_ok(|eventkind| eventkind.is_access())
+        .unwrap_or(false)
 }
 
 #[no_mangle]
-pub extern "C" fn filewatcher_eventkind_is_create(
-    ptr: *mut ValueBox<EventKind>
-) -> bool {
-    match ptr.to_ref() {
-        Ok(eventkind) => eventkind.is_create(),
-        Err(_) => false
-    }
+pub extern "C" fn filewatcher_eventkind_is_create(ptr: *mut ValueBox<EventKind>) -> bool {
+    ptr.with_ref_ok(|eventkind| eventkind.is_create())
+        .unwrap_or(false)
 }
 
 #[no_mangle]
-pub extern "C" fn filewatcher_eventkind_is_modify(
-    ptr: *mut ValueBox<EventKind>
-) -> bool {
-    match ptr.to_ref() {
-        Ok(eventkind) => eventkind.is_modify(),
-        Err(_) => false
-    }
+pub extern "C" fn filewatcher_eventkind_is_modify(ptr: *mut ValueBox<EventKind>) -> bool {
+    ptr.with_ref_ok(|eventkind| eventkind.is_modify())
+        .unwrap_or(false)
 }
 
 #[no_mangle]
-pub extern "C" fn filewatcher_eventkind_is_remove(
-    ptr: *mut ValueBox<EventKind>
-) -> bool {
-    match ptr.to_ref() {
-        Ok(eventkind) => eventkind.is_remove(),
-        Err(_) => false
-    }
+pub extern "C" fn filewatcher_eventkind_is_remove(ptr: *mut ValueBox<EventKind>) -> bool {
+    ptr.with_ref_ok(|eventkind| eventkind.is_remove())
+        .unwrap_or(false)
 }
 
 #[no_mangle]
-pub extern "C" fn filewatcher_eventkind_is_other(
-    ptr: *mut ValueBox<EventKind>
-) -> bool {
-    match ptr.to_ref() {
-        Ok(eventkind) => eventkind.is_other(),
-        Err(_) => false
-    }
+pub extern "C" fn filewatcher_eventkind_is_other(ptr: *mut ValueBox<EventKind>) -> bool {
+    ptr.with_ref_ok(|eventkind| eventkind.is_other())
+        .unwrap_or(false)
 }
 
 #[no_mangle]
-pub extern "C" fn filewatcher_event_path_size(
-    ptr: *mut ValueBox<Event>,
-) -> usize {
-    match ptr.to_ref() {
-        Ok(event) => event.paths.len(),
-        Err(_) => 0
-    }
+pub extern "C" fn filewatcher_event_path_size(ptr: *mut ValueBox<Event>) -> usize {
+    ptr.with_ref_ok(|event| event.paths.len()).unwrap_or(0)
 }
 
 #[no_mangle]
@@ -299,13 +270,10 @@ pub extern "C" fn filewatcher_event_path_at(
     index: usize,
 ) {
     str_ptr
-        .to_ref()
-        .and_then(|mut contents| {
-            ptr
-                .to_ref()
-                .map(|event| {
-                    contents.set_string(event.paths[index].to_string_lossy().to_string())
-                })
+        .with_mut(|contents| {
+            ptr.with_ref_ok(|event| {
+                contents.set_string(event.paths[index].to_string_lossy().to_string())
+            })
         })
         .log();
 }
@@ -313,37 +281,39 @@ pub extern "C" fn filewatcher_event_path_at(
 #[phlow::extensions(FileWatcherExtensions, EventKind)]
 impl EventKindExtensions {
     #[phlow::view]
-    pub fn information_for(_this: &EventKind, view: impl phlow::PhlowView) -> impl phlow::PhlowView {
+    pub fn information_for(
+        _this: &EventKind,
+        view: impl phlow::PhlowView,
+    ) -> impl phlow::PhlowView {
         view.list()
             .title("Information")
-            .items(|event_kind: &EventKind, _object| {
+            .items::<EventKind>(|event_kind| {
                 phlow_all!(vec![
-                    ("Is access", phlow!(event_kind.is_access())),
-                    ("Is modify", phlow!(event_kind.is_modify())),
-                    ("Is create", phlow!(event_kind.is_create())),
-                    ("Is remove", phlow!(event_kind.is_remove())),
-                    ("Is other", phlow!(event_kind.is_other())),
+                    ("Is access", event_kind.is_access()),
+                    ("Is modify", event_kind.is_modify()),
+                    ("Is create", event_kind.is_create()),
+                    ("Is remove", event_kind.is_remove()),
+                    ("Is other", event_kind.is_other()),
                 ])
             })
-            .item_text(|each: &(&str, phlow::PhlowObject), _object| {
+            .item_text::<(&str, bool)>(|each| {
                 format!("{}: {}", each.0, each.1.to_string())
             })
-            .send(|each: &(&str, phlow::PhlowObject), _object| each.1.clone())
+            .send::<(&str, bool)>(|each| phlow!(each.1.clone()))
     }
 }
 
 #[phlow::extensions(FileWatcherExtensions, AffectedPaths)]
 impl AffectedPathExtensions {
     #[phlow::view]
-    pub fn information_for(_this: &AffectedPaths, view: impl phlow::PhlowView) -> impl phlow::PhlowView {
+    pub fn information_for(
+        _this: &AffectedPaths,
+        view: impl phlow::PhlowView,
+    ) -> impl phlow::PhlowView {
         view.list()
             .title("Information")
-            .items(|affected_paths: &AffectedPaths, _object| {
-                phlow_all!(affected_paths.clone())
-            })
-            .item_text(|each: &PathBuf, _object| {
-                each.to_string_lossy().to_string()
-            })
+            .items::<AffectedPaths>(|affected_paths| phlow_all!(affected_paths.clone()))
+            .item_text::<PathBuf>(|each| each.to_string_lossy().to_string())
     }
 }
 
@@ -353,12 +323,12 @@ impl PathBufExtensions {
     pub fn information_for(_this: &PathBuf, view: impl phlow::PhlowView) -> impl phlow::PhlowView {
         view.list()
             .title("Information")
-            .items(|path: &PathBuf, _object| {
-                phlow_all!(vec![("Path", phlow!(path.to_string_lossy().to_string()))])
+            .items::<PathBuf>(|path| {
+                phlow_all!(vec![("Path", path.to_string_lossy().to_string())])
             })
-            .item_text(|each: &(&str, phlow::PhlowObject), _object| {
+            .item_text::<(&str, &str)>(|each| {
                 format!("{}: {}", each.0, each.1.to_string())
             })
-            .send(|each: &(&str, phlow::PhlowObject), _object| each.1.clone())
+            .send::<(&str, &str)>(|each| phlow!(each.1.clone()))
     }
 }
