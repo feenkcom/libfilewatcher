3832527c68d872d4a759f215819fa3e80605eeef
Veit Heller <veit@veitheller.de>
2023-04-28 10:37:48 +0200

    Update to new project structure

--- libfilewatcher-builder/src/main.rs
+++ libfilewatcher-builder/src/main.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 
 use shared_library_builder::build_standalone;
 
-use libfilewatcher_library::latest_libfilewatcher;
+use libfilewatcher_builder::latest_libfilewatcher;
 
 fn main() -> Result<(), Box<dyn Error>> {
     build_standalone(|_| Ok(Box::new(latest_libfilewatcher())))
